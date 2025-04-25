7eec2035a6d1608ee059a20740cdbdd31938a43b
Le-Jr <nascimentoleandrojr@gmail.com>
2025-04-25 17:37:21 -0300

    feat(backend) add docker config

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CategoryService } from './category.service';
 
 @Controller()
-export class CategoryController {}
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  @Get('category')
+  listCategory() {
+    return this.categoryService.listCategory();
+  }
+}
