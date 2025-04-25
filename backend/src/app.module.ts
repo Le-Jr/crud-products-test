25aa95c9055cf57cde3c9d3b0da5a7e82949d1e7
Le-Jr <nascimentoleandrojr@gmail.com>
2025-04-25 16:29:54 -0300

    feat(backend) create category module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { CategoryModule } from './category/category.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CategoryModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
