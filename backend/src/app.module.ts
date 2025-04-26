9eb45596260addc5614ab0cc4e81ea0710d65019
Le-Jr <nascimentoleandrojr@gmail.com>
2025-04-25 21:37:04 -0300

    feat(backend) prisma module config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CategoryModule, PrismaModule, ProductModule],
+  imports: [
+    CategoryModule,
+    PrismaModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/category/category.module.ts
+++ backend/src/category/category.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
