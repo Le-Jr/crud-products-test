a28c8cf2788bb68f0f79d855f477514dbaa41d91
Le-Jr <nascimentoleandrojr@gmail.com>
2025-04-25 22:02:50 -0300

    feat(backend) implement GET /category and POST /category

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { CategoryDto } from 'src/dto';
 
 @Controller()
 export class CategoryController {
@@ -8,5 +9,10 @@ export class CategoryController {
   @Get('category')
   listCategory() {
     return this.categoryService.listCategory();
+  }
+
+  @Post('category')
+  createCategory(@Body() dto: CategoryDto) {
+    return this.categoryService.createCategory(dto);
   }
 }
--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { CategoryDto } from 'src/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class CategoryService {
   constructor(private prisma: PrismaService) {}
-  listCategory() {
-    return 'Hello';
+  async listCategory() {
+    const user = await this.prisma.category.findMany({
+      include: {
+        children: true,
+        parent: true,
+        products: true,
+      },
+    });
+
+    return user;
+  }
+
+  async createCategory(dto: CategoryDto) {
+    const user = await this.prisma.category.create({
+      data: {
+        parentId: dto.parentId,
+        name: dto.name,
+      },
+    });
+    return user;
   }
 }
